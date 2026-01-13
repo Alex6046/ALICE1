50ba2b1f3d3fe413310c180eece2c4d22c9359a2
AezyFkry <91457894+Alex6046@users.noreply.github.com>
2026-01-13 20:56:38 +0800

    vercel

--- api/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>© 2025 ALICE. All rights reserved.</p>
-</footer>
-<script src="script.js"></script>
-</body>
-</html>
--- api/script.js
+++
@@ -1,7 +0,0 @@
-function showSection(id) {
-    document.querySelectorAll('.info-section').forEach(sec => sec.classList.remove('show'));
-    document.getElementById(id).classList.add('show');
-
-    document.querySelectorAll('.btn').forEach(btn => btn.classList.remove('active'));
-    event.target.classList.add('active');
-}
